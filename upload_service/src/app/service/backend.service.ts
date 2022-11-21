5f13a4f4fb850383caa450243401a823cfa7cee4
Ananth Annamalai <ananth.annamalai.19@gmail.com>
2022-11-21 12:41:11 +0530

    Intial Setup for Streaming Service

--- upload_service/src/app/service/backend.service.ts
+++ upload_service/src/app/service/backend.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 import { Movie, UploadResponse } from '../model/movie.mode';
 
 @Injectable({
